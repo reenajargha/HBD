9eb1ed33bae03d45fbb7884f1f43f3bcd5e7de1a
reenajargha <reenajargha@gmail.com>
2026-08-07 01:40:41 +0545

    2nd commit

--- birthday/settings.py
+++ birthday/settings.py
@@ -26,12 +26,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*g"]
+ALLOWED_HOSTS = [
+    "your-project.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://your-project.up.railway.app",
+]
 
 # Application definition
 
